e7430ef6397818e9a54126fd863a6a67374dd95d
Scui <kaka128@gmx.de>
2023-03-08 14:23:04 +0100

    query for InstrumentationCallback (doesn't work)

--- Process/main.cpp
+++ Process/main.cpp
@@ -2,11 +2,34 @@
 #include <Windows.h>
 
 using NtReadVirtualMemory_t = NTSTATUS(NTAPI*)(HANDLE, PVOID, PVOID, SIZE_T, PSIZE_T);
+using NtQueryInformationProcess_t = NTSTATUS(NTAPI*)(HANDLE, int, PVOID, ULONG, PULONG);
+
+using InstrumentationCallback_t = void(*)();
+struct PROCESS_INSTRUMENTATION_CALLBACK_INFORMATION
+{
+	ULONG version;
+	ULONG reserved;
+	InstrumentationCallback_t callback;
+};
 
 int main()
 {
 	std::cout << "Loading dll" << std::endl;
 	system("pause");
+
+	NtQueryInformationProcess_t NtQueryInformationProcess = (NtQueryInformationProcess_t)GetProcAddress(GetModuleHandleW(L"ntdll.dll"), "NtQueryInformationProcess");
+	PROCESS_INSTRUMENTATION_CALLBACK_INFORMATION pi;
+	ULONG someLen = 0;
+	NTSTATUS queryInfoStatus = NtQueryInformationProcess((HANDLE)-1, 0x28, &pi, sizeof(pi), &someLen);
+	if (queryInfoStatus >= 0)
+	{
+		std::cout << "NtQueryInformationProcess suceeded!" << std::endl;
+	}
+	else {
+		std::cout << "NtQueryInformationProcess failed!, error: 0x" << std::hex << queryInfoStatus << std::endl;
+	}
+
+
 	LoadLibraryA("Dll.dll");
 	std::cout << "Dll loaded, calling shit" << std::endl;
 
